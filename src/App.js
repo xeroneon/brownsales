d39aa06a8863c4e35433280d2fa3c2276720a08f
Magic-Mayo <mike.mayoii@gmail.com>
2020-04-01 11:00:21 -0700

    added context for clearance items and contentful api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Clearance from './pages/Clearance';
+import { ClearanceItemsContext } from './Context';
+import contentful from 'contentful';
+
+const contentfulAPI = contentful.createClient({
+    space: process.env.REACT_APP_SPACE,
+    accessToken: process.env.REACT_APP_SECRET
+})
 
 function App() {
+    const [clearanceItems, setClearanceItems] = useState();
+
     return (
         <>
-            <Nav />
-            <Switch>
-                <Route exact path='/'>
-                    <Home />
-                </Route>
-                <Route path='/clearance'>
-                    <Clearance />
-                </Route>
-            </Switch>
-            <Footer />
+            <ClearanceItemsContext.Provider value={{ contentfulAPI, clearanceItems, setClearanceItems }}>
+                <Nav />
+                <Switch>
+                    <Route exact path='/'>
+                        <Home />
+                    </Route>
+                    <Route path='/clearance'>
+                        <Clearance />
+                    </Route>
+                </Switch>
+                <Footer />
+            </ClearanceItemsContext.Provider>
         </>
     );
 }
