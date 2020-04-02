4c57d309b687f152f4ae65fa7eab6b910fd8aea9
Magic-Mayo <mike.mayoii@gmail.com>
2020-04-02 14:11:42 -0700

    added showimage prop and set img src to base64 if it's not in view so it can wait to load image until in view

--- src/components/ItemCards/index.js
+++ src/components/ItemCards/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ItemCard = ({sku, name, imageLink, price, quantity, orgPrice}) => {
+const ItemCard = ({sku, name, imageLink, price, quantity, orgPrice, index, showImg}) => {
 
     return (
         <div className='clearance-item'>
-            <img src={imageLink} alt={name} />
+            <img src={index < showImg ? imageLink : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOMrgcAATsA3BT31OAAAAAASUVORK5CYII='} alt={name} />
             <div className='clearance-item-desc'>
                 <h4>{name}</h4>
                 {sku && <p className='item-sku'>{`ITEM# - ${sku}`}</p>}
