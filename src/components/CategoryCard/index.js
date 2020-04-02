a8b340dd9b4ed61178134cd9ba10b11ae9abc0d7
Magic-Mayo <mike.mayoii@gmail.com>
2020-04-02 14:11:42 -0700

    changed button back to link

--- src/components/CategoryCard/index.js
+++ src/components/CategoryCard/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 const categories = [
-    {name: 'Mattresses', src: '/img/mattress-image.png'},
+    {name: 'Mattress', src: '/img/mattress-image.png'},
     {name: 'Carpet', src: '/img/carpet-image.png'},
     {name: 'Tile', src: '/img/tile-image.png'},
     {name: 'Granite & Quartz', src: '/img/granite-image.png'},
     {name: 'Lighting', src: '/img/lighting-image.png'},
-    {name: 'Ceiling Fans', src: '/img/fan-image.png'},
-    {name: 'Furniture', src: '/img/furniture-image.png'}
+    {name: 'Ceiling Fans', src: '/img/fan-image.png'}
 ];
 
-const CategoryCards = ({setCategory, onClick}) => {
+const CategoryCards = () => {
     const location = useLocation();
 
     return (
         <div className={`clearance-categories ${location.pathname.split('/')[1] === 'clearance' ? 'clearance' : ''}`}>
             {categories.map((cat, ind) => (
-                <button className='category-card' onClick={onClick ? () => setCategory(cat.name) : null} key={ind}>
+                <Link to={`/clearance/${cat.name.toLowerCase().split(' ').join('-')}`}
+                className='category-card'
+                key={ind}
+                >
                     <img className='category-link' src={cat.src} alt={`${cat.name} Clearance Items`} />
-                </button>
+                </Link>
             ))}
         </div>
 
