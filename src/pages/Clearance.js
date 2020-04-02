46fb8a79dca450ae5bff72f27bf165e68c3a46b2
Magic-Mayo <mike.mayoii@gmail.com>
2020-04-02 14:11:42 -0700

    added react spinner to use when loading data in. added font awesome icon for load more button. made useeffect to set current category to what's in pathname and set showimage back to default. added conditional p tag to let user know items in that category aren't available. conditional render of load more button based on if there are more items to load

--- src/pages/Clearance.js
+++ src/pages/Clearance.js
@@ -2,38 +2,55 @@ import React, { useState, useEffect, useContext } from 'react';
 import { ClearanceItemsContext } from '../Context';
 import CategoryCards from '../components/CategoryCard'
 import ItemCard from '../components/ItemCards';
+import { useLocation } from 'react-router-dom';
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon as FAIcon } from '@fortawesome/react-fontawesome';
+import { ClipLoader as Loading } from 'react-spinners';
 
 const Clearance = () => {
     const { contentfulAPI, clearanceItems, setClearanceItems } = useContext(ClearanceItemsContext);
-    const [category, setCategory] = useState('all');
+    const [currentCategory, setCurrentCategory] = useState();
+    const [showImg, setShowImg] = useState(6);
+    const location = useLocation();
 
     useEffect(() => {
         // Get items from contentful
-        contentfulAPI.getEntries().then(entries => setClearanceItems(entries.items));
+        contentfulAPI.getEntries().then(entries => {
+            setClearanceItems(entries.items)
+            if(location.pathname.split('/')[2]){
+                setCurrentCategory(location.pathname.split('/')[2].split('-').join(' ').toLowerCase());
+            }
+        });
     }, []);
+
+    useEffect(() => {
+        setShowImg(6);
+        setCurrentCategory(location.pathname.split('/')[2].split('-').join(' ').toLowerCase());
+    }, [location.pathname]);
 
     return (
         <>
-
             <div className='home-clearance-header'>
                 <h2>Clearance Items In Our Phoenix Warehouse</h2>
                 <p>Up to 75% off all clearance items</p>
             </div>
 
             <CategoryCards
-            setCategory={setCategory}
-            onClick
+            setCurrentCategory={setCurrentCategory}
             />
 
-            <div className='clearance-items-wrapper'>
+            <div
+            className='clearance-items-wrapper'
+            style={{height: `${showImg * 150}px`}}
+            >
 
-                {clearanceItems &&
+                {clearanceItems ?
                     clearanceItems
-                    .filter(item => category === 'all' ? item : item.fields.category.toLowerCase() === category.toLowerCase())
-                    .map(item => (
+                    .filter(item => currentCategory ? item.fields.category.toLowerCase() === currentCategory : item)
+                    .map((item, ind) => (
 
                         <ItemCard
-                        key={item.fields.sku}
+                        key={ind}
                         price={item.fields.price}
                         name={item.fields.name}
                         desc={item.fields.description}
@@ -41,10 +58,29 @@ const Clearance = () => {
                         quantity={item.fields.quantity}
                         imageLink={item.fields.imageLink}
                         orgPrice={item.fields.orgPrice}
+                        index={ind}
+                        showImg={showImg}
                         />
                     ))
-                    // :
-                    // null
+                :
+                    <Loading
+                    loading={true}
+                    />
+                
+                }
+                {clearanceItems && clearanceItems.filter(item => item.fields.category.toLowerCase() === currentCategory).length < 1 && 
+                    <p className='clearance-out'>All {currentCategory} clearance items currently sold out!</p>
+                }
+                {clearanceItems && clearanceItems.filter(item => currentCategory ? item.fields.category.toLowerCase() === currentCategory : item).length > showImg &&
+                    <span
+                    className='clearance-load'
+                    onClick={() => setShowImg(clearanceItems.filter(item => item.fields.category.toLowerCase() === currentCategory).length % 6 < 4 ? showImg + 3 : showImg + 6)}
+                    >
+                        <span>Load More</span>
+                        <FAIcon
+                        icon={faAngleDoubleDown}
+                        />
+                    </span>
                 }
             </div>
         </>
