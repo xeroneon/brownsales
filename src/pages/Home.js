0703abe37c8343f778b4f72afb3e6c8a7c0c5d0b
Magic-Mayo <mike.mayoii@gmail.com>
2020-04-01 18:36:23 -0700

    added category cards and class name for header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import CategoryCard from '../components/CategoryCard';
 
 const Home = () => {
 
     return (
         <>
-            
+            <div className='home-clearance-header'>
+                <h2>Clearance Items In Our Phoenix Warehouse</h2>
+                <p>Up to 75% off all clearance items</p>
+            </div>
+
+            <CategoryCard />
         </>
     )
 }
