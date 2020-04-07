f80a2297b6b98be879d6eacc8447ae3fe96a7940
Magic-Mayo <mike.mayoii@gmail.com>
2020-04-07 16:27:24 -0700

    added in scroll effect for page transition. scrolls to top of clearance header element. set showImage state to conditional number on render based on how many items are in the category. highest is 6 but if less than 4 items will be 3

--- src/pages/Clearance.js
+++ src/pages/Clearance.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import { ClearanceItemsContext } from '../Context';
 import CategoryCards from '../components/CategoryCard'
 import ItemCard from '../components/ItemCards';
@@ -9,44 +9,57 @@ import { ClipLoader as Loading } from 'react-spinners';
 
 const Clearance = () => {
     const { contentfulAPI, clearanceItems, setClearanceItems } = useContext(ClearanceItemsContext);
-    const [currentCategory, setCurrentCategory] = useState();
+    const [category, setCategory] = useState();
     const [showImg, setShowImg] = useState(6);
     const location = useLocation();
+    const clearanceHeaderRef = useRef(null)
 
     useEffect(() => {
         // Get items from contentful
         contentfulAPI.getEntries().then(entries => {
             setClearanceItems(entries.items)
             if(location.pathname.split('/')[2]){
-                setCurrentCategory(location.pathname.split('/')[2].split('-').join(' ').toLowerCase());
+                setCategory(location.pathname.split('/')[2].split('-').join(' ').toLowerCase());
             }
         });
     }, []);
+    
+    useEffect(() => {
+        setCategory(location.pathname.split('/')[2].split('-').join(' ').toLowerCase());
+        if(clearanceItems)
+            setShowImg(() => {
+                const images = clearanceItems.filter(item => item.fields.category.toLowerCase() === category)
+                return images.length > 3 ? 6 : 3;
+            });
+    }, [location.pathname, clearanceItems, category]);
 
     useEffect(() => {
-        setShowImg(6);
-        setCurrentCategory(location.pathname.split('/')[2].split('-').join(' ').toLowerCase());
-    }, [location.pathname]);
+        window.scrollTo({top: clearanceHeaderRef.current.offsetTop, behavior: 'smooth'})
+    }, [])
 
     return (
         <>
-            <div className='home-clearance-header'>
+            <div
+            className='home-clearance-header'
+            ref={clearanceHeaderRef}
+            >
                 <h2>Clearance Items In Our Phoenix Warehouse</h2>
                 <p>Up to 75% off all clearance items</p>
             </div>
 
             <CategoryCards
-            setCurrentCategory={setCurrentCategory}
+            setCurrentCategory={setCategory}
             />
 
             <div
-            className='clearance-items-wrapper'
+            className={`clearance-items-wrapper ${clearanceItems ? '' : 'loading'}`}
             style={{height: `${showImg * 150}px`}}
+            
             >
 
                 {clearanceItems ?
                     clearanceItems
-                    .filter(item => currentCategory ? item.fields.category.toLowerCase() === currentCategory : item)
+                    .filter(item => category ? item.fields.category.toLowerCase() === category : item)
                     .filter(item => item.fields.quantity > 0)
                     .map((item, ind) => (
 
@@ -66,16 +79,17 @@ const Clearance = () => {
                 :
                     <Loading
                     loading={true}
+                    size={80}
                     />
                 
                 }
-                {clearanceItems && clearanceItems.filter(item => item.fields.category.toLowerCase() === currentCategory).length < 1 && 
-                    <p className='clearance-out'>All {currentCategory} clearance items currently sold out!</p>
+                {clearanceItems && clearanceItems.filter(item => item.fields.category.toLowerCase() === category).length < 1 && 
+                    <p className='clearance-out'>All {category} clearance items currently sold out!</p>
                 }
-                {clearanceItems && clearanceItems.filter(item => currentCategory ? item.fields.category.toLowerCase() === currentCategory : item).length > showImg &&
+                {clearanceItems && clearanceItems.filter(item => category ? item.fields.category.toLowerCase() === category : item).length > showImg &&
                     <span
                     className='clearance-load'
-                    onClick={() => setShowImg(clearanceItems.filter(item => item.fields.category.toLowerCase() === currentCategory).length % 6 < 4 ? showImg + 3 : showImg + 6)}
+                    onClick={() => setShowImg(clearanceItems.filter(item => item.fields.category.toLowerCase() === category).length % 6 < 4 ? showImg + 3 : showImg + 6)}
                     >
                         <span>Load More</span>
                         <FAIcon
