21fb16d013ad19a83e8071ac0849b4cfb098ef0c
Magic-Mayo <mike.mayoii@gmail.com>
2020-04-07 17:28:00 -0700

    removed category array so it can be set based on props instead and component can be more reusable

--- src/components/CategoryCard/index.js
+++ src/components/CategoryCard/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
 
-const categories = [
-    {name: 'Mattress', src: '/img/mattress-image.png'},
-    {name: 'Carpet', src: '/img/carpet-image.png'},
-    {name: 'Tile', src: '/img/tile-image.png'},
-    {name: 'Granite & Quartz', src: '/img/granite-image.png'},
-    {name: 'Lighting', src: '/img/lighting-image.png'},
-    {name: 'Ceiling Fans', src: '/img/fan-image.png'}
-];
-
-const CategoryCards = () => {
+const CategoryCards = ({categories}) => {
     const location = useLocation();
+    const path = location.pathname.split('/');
 
     return (
-        <div className={`clearance-categories ${location.pathname.split('/')[1] === 'clearance' ? 'clearance' : ''}`}>
+        <div className={`clearance-categories ${path[1] === 'clearance' ? 'clearance' : ''}`}>
             {categories.map((cat, ind) => (
                 <Link
-                to={`/clearance/${cat.name.toLowerCase().split(' ').join('-')}`}
+                to={`/${path[1] === 'stock' ? 'stock' : 'clearance'}/${cat.name.toLowerCase().split(' ').join('-')}`}
                 className='category-card'
                 key={ind}
                 >
