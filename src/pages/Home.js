5ba96533b4d381a7df2ab553392daaaf62121991
Magic-Mayo <mike.mayoii@gmail.com>
2020-04-10 14:23:58 -0700

    removed src in categories array since images are imported directly to components now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,12 @@ import CategoryCard from '../components/CategoryCard';
 import { Link } from 'react-router-dom';
 
 const categories = [
-    {name: 'Mattress', src: '/img/mattress-image.png'},
-    {name: 'Carpet', src: '/img/carpet-image.png'},
-    {name: 'Tile', src: '/img/tile-image.png'},
-    {name: 'Granite & Quartz', src: '/img/granite-image.png'},
-    {name: 'Lighting', src: '/img/lighting-image.png'},
-    {name: 'Ceiling Fans', src: '/img/fan-image.png'}
+    {name: 'Mattress'},
+    {name: 'Carpet'},
+    {name: 'Tile'},
+    {name: 'Granite & Quartz'},
+    {name: 'Lighting'},
+    // {name: 'Ceiling Fans', src: fans}
 ];
 
 const Home = () => {
