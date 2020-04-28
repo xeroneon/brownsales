f82e5adbba94b8e7421dc7ee898c6ef2369e1afb
Magic-Mayo <mike.mayoii@gmail.com>
2020-04-27 18:34:06 -0700

    moved fb icon to contact component. moved where brands comp is rendered to be outside of footer element

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,31 +2,25 @@ import React from 'react';
 import Brands from './Brands';
 import Hours from './Hours';
 import Contact from './Contact';
-import facebook from '../../img/facebook.png'
 
 const Footer = () => {
 
     return (
-        <footer>
-
+        <>
+        
             <Brands />
-            <Hours />
-            <Contact />
 
-            <div className='footer'>
-                <p>&copy; 2019 Brown Sales, Inc.  All Rights Reserved</p>
-                <div className='footer-links'>
-                    <a
-                    href='https://www.facebook.com/pages/Brown-Sales/162169013801027'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    >
-                        <img src={facebook} alt='Facebook' />
-                    </a>
+            <footer>
+
+                <Hours />
+                <Contact />
+
+                <div className='footer'>
                     <a href='/#' >Privacy Policy</a>
+                    <p>&copy; 2019 Brown Sales, Inc.  All Rights Reserved</p>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </>
     )
 }
 
