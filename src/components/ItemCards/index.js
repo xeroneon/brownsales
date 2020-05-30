d48dcc90125ce73f76d5ebfbbd9a8422d96ae664
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-29 22:29:49 -0700

    render modal conditionally and used state to trigger the modal

--- src/components/ItemCards/index.js
+++ src/components/ItemCards/index.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+import Modal from '../Modal';
 
-const ItemCard = ({ name, imageLink, price, last }) => {
+const ItemCard = ({ name, imageLink, price, description }) => {
   const location = useLocation();
   const pathname = location.pathname.split("/");
+  const [modal, setModal] = useState(false);
 
   return (
-    <div className={`${pathname[1]}-item product`} ref={last}>
-      <div className="product--image__container">
-        <img src={imageLink} alt={name} className="product--image" />
-      </div>
-      <div className="product--info">
-        <h4 className="product--name">{name}</h4>
-        <h5 className="product--price">${price}</h5>
-      </div>
-    </div>
+    <>
+        {modal &&
+            <Modal
+            setModal={setModal}
+            >
+                <div className='modal' style={{zIndex: 2}}>
+                    <div className="image__container__modal">
+                        <img src={imageLink} alt={name} className="product--image__modal" />
+                    </div>
+                    <div className="product--info__modal">
+                        <h4 className="product--name__modal">{name}</h4>
+                        <h5 className="product--price__modal">${price}</h5>
+                        <p className="product--description__modal">{description}</p>
+                    </div>
+                </div>
+            </Modal>
+        }
+
+        <div className={`${pathname[1]}-item product`} onClick={() => setModal(true)}>
+            <div className="product--image__container">
+                <img src={imageLink} alt={name} className="product--image" />
+            </div>
+            <div className="product--info">
+                <h4 className="product--name">{name}</h4>
+                <h5 className="product--price">${price}</h5>
+            </div>
+        </div>
+    </>
   );
 };
 
