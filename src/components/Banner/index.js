0742a1a280ff408bee6095f271ef1f9e7aded818
Sean <1996.sean.alexander@gmail.com>
2020-06-01 04:35:20 -0700

    styled banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -14,7 +14,7 @@ const Banner = ({contentfulAPI}) => {
         <>
             {bannerText &&
                 // This will be limited to 500 characters
-                <span>{bannerText}</span>
+                <span class="banner">{bannerText}</span>
             }
         </>
     )
