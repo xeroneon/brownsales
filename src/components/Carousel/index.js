a9b154ed6ac41caaf645d1a1e18f89e2d6dc7412
Magic-Mayo <mike.mayoii@gmail.com>
2020-06-02 14:03:11 -0700

    created image array with image link and alt name and mapped that instead of creating separate elements. will be able to pass images thru props this way and component can be used for any number of images instead of a finite amount

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -3,27 +3,46 @@ import apartment from '../../img/apartment-carousel.jpg';
 import carpetWarehouse from '../../img/carpet-warehouse.jpg';
 import warehouse from '../../img/warehouse.jpeg';
 import building from '../../img/building.jpg';
+import flooring from '../../img/flooring.jpg';
+
+const imgArr = [
+    {image:apartment, name: 'apartment'},
+    {image: carpetWarehouse, name: 'carpet warehouse'},
+    {image: warehouse, name: 'warehouse'},
+    {image: building, name: 'building'},
+    {image: flooring, name: 'flooring'}
+]
 
 const Carousel = () => {
-    const [carouselImg, setCarouselImg] = useState(1);
+    const [carouselImg, setCarouselImg] = useState(0);
 
     useEffect(() => {
         const carouselInterval = setInterval(() => {
             setCarouselImg(prevImg => {
-                if(prevImg < 4) return prevImg + 1;
-                if(prevImg === 4) return 1;
+                if(prevImg < imgArr.length - 1) return prevImg + 1;
+                if(prevImg === imgArr.length - 1) return 0;
             });
-        }, 7000);
+        }, 5000);
 
         return () => clearInterval(carouselInterval);
     })
 
     return (
-        <div className='image__carousel'>
-            <img src={apartment} alt='apartment' className={`image--one__carousel ${carouselImg === 1 ? 'current' : carouselImg === 2 ? 'inactive' : ''}`}></img>
-            <img src={carpetWarehouse} alt='carpet warehouse' className={`image--two__carousel  ${carouselImg === 2 ? 'current' : carouselImg === 3 ? 'inactive' : ''}`}></img>
-            <img src={warehouse} alt='warehouse' className={`image--three__carousel  ${carouselImg === 3 ? 'current' : carouselImg === 4 ? 'inactive' : ''}`}></img>
-            <img src={building} alt='building' className={`image--four__carousel  ${carouselImg === 4 ? 'current' : carouselImg === 1 ? 'inactive' : ''}`}></img>
+        <div className='carousel__image--container'>
+            {imgArr.map((img, ind) =>
+                <img
+                src={img.image}
+                alt={img.name}
+                className={`carousel__image--one
+                    ${carouselImg === ind ? 'current' : carouselImg === ind + 1 ? 'inactive' : ''}
+                    ${ind === imgArr.length - 1 && carouselImg === 0 ? 'inactive' : ''}`}
+                />
+            )}
+            
+            <div className='carousel__heading--container'>
+                <h4 className='carousel__heading'> Brown Sales Flooring specializes in replacement flooring for the multi-family housing industry in the Phoenix Area</h4>
+                <a href='https://www.brownsalesaz.com' rel='noopener noreferrer' target='_blank'>brownsalesaz.com</a>
+            </div>
         </div>
     )
 }
