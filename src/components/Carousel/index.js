737b3b303c99222ffa29d98d62d7d20d1e6cb1ea
Magic-Mayo <mike.mayoii@gmail.com>
2020-06-02 15:04:50 -0700

    moved wrapper to new container component and moved heading to children of container

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -15,22 +15,18 @@ const Carousel = ({images}) => {
     })
 
     return (
-        <div className='carousel__image--container'>
+        <>
             {images.map((img, ind) =>
                 <img
                 src={img.image}
                 alt={img.name}
                 className={`carousel__image--one
-                    ${carouselImg === ind ? 'current' : carouselImg === ind + 1 ? 'inactive' : ''}
+                    ${carouselImg === ind ? 'current' : ''}
+                    ${carouselImg === ind + 1 ? 'inactive' : ''}
                     ${ind === images.length - 1 && carouselImg === 0 ? 'inactive' : ''}`}
                 />
             )}
-
-            <div className='carousel__heading--container'>
-                <h4 className='carousel__heading'> Brown Sales Flooring specializes in replacement flooring for the multi-family housing industry in the Phoenix Area</h4>
-                <a href='https://www.brownsalesaz.com' rel='noopener noreferrer' target='_blank'>brownsalesaz.com</a>
-            </div>
-        </div>
+        </>
     )
 }
 
