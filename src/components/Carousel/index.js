4feb38aebb6c6c4dace1969769de4b06ca74927e
Magic-Mayo <mike.mayoii@gmail.com>
2020-06-02 14:19:36 -0700

    moved image imports and constructed image array to app.js. mapped props coming in instead of image array that used to be here

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,26 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import apartment from '../../img/apartment-carousel.jpg';
-import carpetWarehouse from '../../img/carpet-warehouse.jpg';
-import warehouse from '../../img/warehouse.jpeg';
-import building from '../../img/building.jpg';
-import flooring from '../../img/flooring.jpg';
 
-const imgArr = [
-    {image:apartment, name: 'apartment'},
-    {image: carpetWarehouse, name: 'carpet warehouse'},
-    {image: warehouse, name: 'warehouse'},
-    {image: building, name: 'building'},
-    {image: flooring, name: 'flooring'}
-]
-
-const Carousel = () => {
+const Carousel = ({images}) => {
     const [carouselImg, setCarouselImg] = useState(0);
 
     useEffect(() => {
         const carouselInterval = setInterval(() => {
             setCarouselImg(prevImg => {
-                if(prevImg < imgArr.length - 1) return prevImg + 1;
-                if(prevImg === imgArr.length - 1) return 0;
+                if(prevImg < images.length - 1) return prevImg + 1;
+                if(prevImg === images.length - 1) return 0;
             });
         }, 5000);
 
@@ -29,16 +16,16 @@ const Carousel = () => {
 
     return (
         <div className='carousel__image--container'>
-            {imgArr.map((img, ind) =>
+            {images.map((img, ind) =>
                 <img
                 src={img.image}
                 alt={img.name}
                 className={`carousel__image--one
                     ${carouselImg === ind ? 'current' : carouselImg === ind + 1 ? 'inactive' : ''}
-                    ${ind === imgArr.length - 1 && carouselImg === 0 ? 'inactive' : ''}`}
+                    ${ind === images.length - 1 && carouselImg === 0 ? 'inactive' : ''}`}
                 />
             )}
-            
+
             <div className='carousel__heading--container'>
                 <h4 className='carousel__heading'> Brown Sales Flooring specializes in replacement flooring for the multi-family housing industry in the Phoenix Area</h4>
                 <a href='https://www.brownsalesaz.com' rel='noopener noreferrer' target='_blank'>brownsalesaz.com</a>
