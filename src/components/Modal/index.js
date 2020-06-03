74d972203d47ade3170f55b6822270762587ffd9
Magic-Mayo <mike.mayoii@gmail.com>
2020-06-02 23:07:38 -0700

    made hook and useEffect to capture window width when modal is open and display a button to close the modal when window width is less than 770px

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 const Modal = ({ children, setModal }) => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    console.log(windowWidth)
+
+    useEffect(() => {
+        const windowListener = window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
+
+        return () => window.removeEventListener('resize', windowListener);
+    }, [])
 
     return (
         <>
+            {windowWidth < 770 &&
+                <span className='modal__close' onClick={() => setModal(false)}>X</span>
+            }
             <div className='modal__overlay' onClick={() => setModal(false)}></div>
             {children}
         </>
