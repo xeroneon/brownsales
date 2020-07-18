084aa1dd9a611166a547c650d4c5ebd31bbbc566
Magic-Mayo <mike.mayoii@gmail.com>
2020-07-17 18:59:56 -0700

    added height and width styling for more reusability

--- src/components/Carousel/Container.js
+++ src/components/Carousel/Container.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Carousel from '.';
 
-const CarouselContainer = ({children, images, timeActive}) => {
+const CarouselContainer = ({children, images, timeActive, width, height}) => {
 
     return (
-        <div className='carousel__image--container'>
+        <div className='carousel__image--container' style={{width: width, height: height}}>
             <Carousel
             images={images}
             timeActive={timeActive}
+            width={width}
             />
             {children}
         </div>
